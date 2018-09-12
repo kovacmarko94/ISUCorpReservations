fa59326b95a02f88e5eb963c5945ddab1776b483
Marko Kovac <marko-kovac@simple-task.com>
2018-09-12 15:53:05 +0200

    adding table and pagination to the reservation list page

--- src/app/reservation/reservation-list/reservation-list.component.ts
+++ src/app/reservation/reservation-list/reservation-list.component.ts
@@ -7,6 +7,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationListComponent implements OnInit {
 
+  headers = [
+    'Contact name',
+    'Phone number',
+    'Birth date',
+    'Contact type',
+    ''
+  ];
+
+  reservations = [
+    {
+      contactName: 'test',
+      phoneNumber: 'test',
+      birthDate: 'test',
+      contactType: 'test'
+    },
+    {
+      contactName: 'test',
+      phoneNumber: 'test',
+      birthDate: 'test',
+      contactType: 'test'
+    },
+    {
+      contactName: 'test',
+      phoneNumber: 'test',
+      birthDate: 'test',
+      contactType: 'test'
+    },
+    {
+      contactName: 'test',
+      phoneNumber: 'test',
+      birthDate: 'test',
+      contactType: 'test'
+    },
+    {
+      contactName: 'test',
+      phoneNumber: 'test',
+      birthDate: 'test',
+      contactType: 'test'
+    },
+    {
+      contactName: 'test',
+      phoneNumber: 'test',
+      birthDate: 'test',
+      contactType: 'test'
+    },
+    {
+      contactName: 'test',
+      phoneNumber: 'test',
+      birthDate: 'test',
+      contactType: 'test'
+    },
+    {
+      contactName: 'test',
+      phoneNumber: 'test',
+      birthDate: 'test',
+      contactType: 'test'
+    },
+    {
+      contactName: 'test',
+      phoneNumber: 'test',
+      birthDate: 'test',
+      contactType: 'test'
+    },
+    {
+      contactName: 'test',
+      phoneNumber: 'test',
+      birthDate: 'test',
+      contactType: 'test'
+    }
+  ];
+
+  getRowBackgroundColor = (id) => {
+    return id % 2 === 0 ? '#EEEEEE' : '';
+  }
+
+  reservationObjectKeys = [
+    'contactName',
+    'phoneNumber',
+    'birthDate',
+    'contactType'
+  ];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ReservationListComponent } from './reservation-list/reservation-list.component';
 import { ReservationAddEditComponent } from './reservation-add-edit/reservation-add-edit.component';
 import { ReservationRoutingModule } from './reservation.routing';
-
+import { PaginationModule } from 'ngx-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
-    ReservationRoutingModule
+    ReservationRoutingModule,
+    PaginationModule.forRoot()
   ],
   declarations: [
     ReservationListComponent,
